38d48072cfdc59f351b7116ca63d5cfc14ede18d
Alex <sadovsky.aleksander@gmail.com>
2020-07-29 18:02:35 +0200

    scss font adjustment

--- src/components/Homepage/Contact.js
+++ src/components/Homepage/Contact.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
-    return (
-        <div className="contact">
-            <div className="contact-title">That is just a project<span className="dot">...</span></div>
-            <div className="contact-subtitle">Do you want to see what I'm truly capable of<span className="dot">?</span></div>
-            <div className="contact-email">alexsadowski.dev@gmail.com</div>
+  return (
+    <div className="contact">
+      <div className="container">
+        <div className="contact-title">
+          That is just a project<span className="dot">...</span>
         </div>
-    )
-}
+        <div className="contact-subtitle">
+          Do you want to see what I'm truly capable of
+          <span className="dot">?</span>
+        </div>
+        <div className="contact-email">alexsadowski.dev@gmail.com</div>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
