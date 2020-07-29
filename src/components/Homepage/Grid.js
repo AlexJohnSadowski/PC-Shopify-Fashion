0f60bbeec7784a21e53acd30157d92564c6ca66c
Alex <sadovsky.aleksander@gmail.com>
2020-07-29 18:15:53 +0200

    css divisions-comments created

--- src/components/Homepage/Grid.js
+++ src/components/Homepage/Grid.js
@@ -1,21 +1,11 @@
 import React from "react";
-
-import photo1 from '../../images/photo1.jpg'
-
-import photo2 from '../../images/photo2.jpg'
-
-import photo3 from '../../images/photo3.jpg'
-
-import photo4 from '../../images/photo4.jpg'
-
-import photo5 from '../../images/photo5.jpg'
-
-import photo6 from '../../images/photo6.jpg'
-
-import photo7 from '../../images/photo7.jpg'
-
-import photo8 from '../../images/photo8.jpg'
-
+import photo2 from "../../images/photo2.jpg";
+import photo3 from "../../images/photo3.jpg";
+import photo4 from "../../images/photo4.jpg";
+import photo5 from "../../images/photo5.jpg";
+import photo6 from "../../images/photo6.jpg";
+import photo7 from "../../images/photo7.jpg";
+import photo8 from "../../images/photo8.jpg";
 
 const Grid = () => {
   return (
@@ -25,12 +15,10 @@ const Grid = () => {
           <img src={photo6} />
           <img src={photo5} />
           <img src={photo8} />
-          
         </div>
         <div class="column">
           <img src={photo3} />
           <img src={photo4} />
-          
         </div>
         <div class="column">
           <img src={photo7} />
